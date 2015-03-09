03385be9bcd2b562ab8427632e01020eea11ee2b
Allen Vanderlinde <allen.vanderlinde@gmail.com>
2015-03-09 18:26:00 -0400

    cbu
    layout update

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "07";
+	static const char DATE[] = "09";
 	static const char MONTH[] = "03";
 	static const char YEAR[] = "2015";
 	static const char UBUNTU_VERSION_STYLE[] =  "15.03";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 3;
-	static const long BUILD  = 77;
-	static const long REVISION  = 99;
+	static const long BUILD  = 78;
+	static const long REVISION  = 0;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2620;
-	#define RC_FILEVERSION 1,3,77,99
-	#define RC_FILEVERSION_STRING "1, 3, 77, 99\0"
-	static const char FULLVERSION_STRING [] = "1.3.77.99";
+	static const long BUILDS_COUNT  = 2622;
+	#define RC_FILEVERSION 1,3,78,0
+	#define RC_FILEVERSION_STRING "1, 3, 78, 0\0"
+	static const char FULLVERSION_STRING [] = "1.3.78.0";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 80;
+	static const long BUILD_HISTORY  = 81;
 	
 
 }
