8af1ce5c9245057e23e87abbfa346e2243e97598
Allen Vanderlinde <allen.vanderlinde@gmail.com>
2015-03-17 18:35:24 -0400

    cbu

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 4;
-	static const long BUILD  = 82;
-	static const long REVISION  = 17;
+	static const long BUILD  = 83;
+	static const long REVISION  = 21;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2828;
-	#define RC_FILEVERSION 1,4,82,17
-	#define RC_FILEVERSION_STRING "1, 4, 82, 17\0"
-	static const char FULLVERSION_STRING [] = "1.4.82.17";
+	static const long BUILDS_COUNT  = 2830;
+	#define RC_FILEVERSION 1,4,83,21
+	#define RC_FILEVERSION_STRING "1, 4, 83, 21\0"
+	static const char FULLVERSION_STRING [] = "1.4.83.21";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 86;
+	static const long BUILD_HISTORY  = 87;
 	
 
 }
