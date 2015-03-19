ed02b30ecd93f7c5f79b7a0911257a3236b7c081
Allen Vanderlinde <allen.vanderlinde@gmail.com>
2015-03-19 09:30:17 -0400

    Revert "lambda"
    This reverts commit 8d4072558ea472324e0aaed8558d28885b1c80e4.

--- app/CAppFrame.cpp
+++ app/CAppFrame.cpp
@@ -19,8 +19,6 @@
 #include "../dialogs/CDialogChangeCredentials.h"
 #include "../dialogs/CDialogSetPathToTools.h"
 
-#include <thread>
-
 using namespace std;
 
 
@@ -107,13 +105,9 @@ void CAppFrame::ChangeDefaultCredentials(wxCommandEvent& event) {
     CAppFrame::LaunchPuTTY
 */
 void CAppFrame::LaunchPuTTY(wxCommandEvent& event ) {
-    /* testing out some lambda stuff. */
-    auto func = [](){ wxMessageBox("This was called from a lambda function!", "lambda", wxOK); };
-
-    func();
-
     wxString szArgs(L"plink "); // Build new string to use as arguments list
     wxString szScriptPath(L"\"C:\\grepster\\user\\scripts\\grep-test.txt\""); // Call a user-saved grepster script to run on the server
+
 
     szArgs += "-ssh avanderlinde@172.24.52.150 -pw " + Configuration->Password() + " -m " + szScriptPath;
 
@@ -126,6 +120,7 @@ void CAppFrame::LaunchPuTTY(wxCommandEvent& event ) {
 
     szOutput = SpawnAndRun(Configuration->PathToSSHTool(), szArgs);
     *Console << L"\nFinished.";
+
 
     Console->BlueText();
     *Console << L"\n\nRunning command...\n" + Configuration->Username() + L"@grepster> ";
--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "18";
+	static const char DATE[] = "17";
 	static const char MONTH[] = "03";
 	static const char YEAR[] = "2015";
 	static const char UBUNTU_VERSION_STYLE[] =  "15.03";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 4;
-	static const long BUILD  = 87;
-	static const long REVISION  = 31;
+	static const long BUILD  = 83;
+	static const long REVISION  = 21;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2838;
-	#define RC_FILEVERSION 1,4,87,31
-	#define RC_FILEVERSION_STRING "1, 4, 87, 31\0"
-	static const char FULLVERSION_STRING [] = "1.4.87.31";
+	static const long BUILDS_COUNT  = 2830;
+	#define RC_FILEVERSION 1,4,83,21
+	#define RC_FILEVERSION_STRING "1, 4, 83, 21\0"
+	static const char FULLVERSION_STRING [] = "1.4.83.21";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 91;
+	static const long BUILD_HISTORY  = 87;
 	
 
 }
